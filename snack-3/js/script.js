ee279da6ec1183ec75c53462ac8fff68ecb54ce0
Ilaria Mammucari <ilariamammucari@hotmail.it>
2021-01-19 14:44:37 +0100

    snack-3 con argomenti nelle funzioni arrow

--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -1,13 +1,13 @@
 // Utilizzando const e/o let. Dare la possibilità di inserire due parole. Verificare se le due parole hanno la stessa lunghezza. Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
 
 // *****funzioni
-const lunghezza = () => {
-    if ( parola1.length > parola2.length ){
-        console.log(`La parola più lunga è la prima: ${parola1}`);
-    } else if ( parola1.length < parola2.length ){
-        console.log(`La parola più lunga è la seconda: ${parola2}`);
+const lunghezza = (par1,par2) => {
+    if ( par1.length > par2.length ){
+        console.log(`La parola più lunga è la prima: ${par1}`);
+    } else if ( par1.length < par2.length ){
+        console.log(`La parola più lunga è la seconda: ${par2}`);
     } else {
-        console.log(`Le due parole sono lunghe uguali: ${parola1}, ${parola2}`);
+        console.log(`Le due parole sono lunghe uguali: ${par1}, ${par2}`);
     }
 }
 // *****/funzioni
@@ -16,4 +16,4 @@ const lunghezza = () => {
 const parola1 = prompt('Inserisci una parola').trim();
 const parola2 = prompt('Inserisci un\'altra parola').trim();
 
-lunghezza();
+lunghezza(parola1,parola2);
